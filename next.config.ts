075e6029b87867015ad012a588f54862c8d3b96c
Tjark <40314141+Triple1090@users.noreply.github.com>
2026-02-04 22:40:12 +0100

    htaccss

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',      // Das hier ist der magische Schalter!
+  output: "export", // Das hier ist der magische Schalter!
+  trailingSlash: false,
   images: {
-    unoptimized: true,   // Wichtig, da Lima-City Bilder nicht live berechnen kann
+    unoptimized: true, // Wichtig, da Lima-City Bilder nicht live berechnen kann
   },
 };
 
